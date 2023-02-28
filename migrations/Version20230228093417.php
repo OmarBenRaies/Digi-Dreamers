dbbfb534df6e5b1bed00c0563716fd3b6758c9bf
Nour <nour.benabderrahmen@esprit.tn>
2023-02-28 10:55:21 +0100

    fix merge issues

---
+++ migrations/Version20230228093417.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230228093417 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE commentaire ADD publication_id INT NOT NULL');
+        $this->addSql('ALTER TABLE commentaire ADD CONSTRAINT FK_67F068BC38B217A7 FOREIGN KEY (publication_id) REFERENCES publication (id)');
+        $this->addSql('CREATE INDEX IDX_67F068BC38B217A7 ON commentaire (publication_id)');
+        $this->addSql('ALTER TABLE user DROP image, DROP updated_at');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE commentaire DROP FOREIGN KEY FK_67F068BC38B217A7');
+        $this->addSql('DROP INDEX IDX_67F068BC38B217A7 ON commentaire');
+        $this->addSql('ALTER TABLE commentaire DROP publication_id');
+        $this->addSql('ALTER TABLE user ADD image VARCHAR(255) DEFAULT \'\', ADD updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+    }
+}
