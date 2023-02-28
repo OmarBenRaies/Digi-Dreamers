40639ee51abe59e8ed445ae97ca6e6453e4a6032
Nour <nour.benabderrahmen@esprit.tn>
2023-02-28 10:26:33 +0100

    fix HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends AbstractController
 {
 
     #[Route('/home',name:'app_home')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
         $repository= $doctrine->getRepository(Publication::class);
     $publications=$repository->findAll();
