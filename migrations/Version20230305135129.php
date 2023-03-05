40d55105652f9f79010c8b0e1acd8bd84b7b30b8
Nour <nour.benabderrahmen@esprit.tn>
2023-03-05 14:52:35 +0100

    migrations files

--- migrations/Version20230305135129.php
+++ migrations/Version20230305135129.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230303223119 extends AbstractMigration
+final class Version20230305135129 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,8 +21,9 @@ final class Version20230303223119 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE commentaire (id INT AUTO_INCREMENT NOT NULL, publication_id INT NOT NULL, contenu_comm VARCHAR(255) NOT NULL, date_com DATE NOT NULL, INDEX IDX_67F068BC38B217A7 (publication_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE pub_like (id INT AUTO_INCREMENT NOT NULL, publication_id INT NOT NULL, user_id INT DEFAULT NULL, INDEX IDX_57AD89138B217A7 (publication_id), INDEX IDX_57AD891A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE publication (id INT AUTO_INCREMENT NOT NULL, contenu_pub VARCHAR(255) NOT NULL, date_pub DATE NOT NULL, code_pub VARCHAR(255) NOT NULL, url_image_pub VARCHAR(255) NOT NULL, all_day TINYINT(1) NOT NULL, background_color VARCHAR(7) NOT NULL, border_color VARCHAR(7) NOT NULL, text_color VARCHAR(7) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, telephone VARCHAR(255) NOT NULL, cin VARCHAR(255) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', verified INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, telephone VARCHAR(255) NOT NULL, cin VARCHAR(255) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', verified INT NOT NULL, image VARCHAR(255) DEFAULT \'\', updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE commentaire ADD CONSTRAINT FK_67F068BC38B217A7 FOREIGN KEY (publication_id) REFERENCES publication (id)');
         $this->addSql('ALTER TABLE pub_like ADD CONSTRAINT FK_57AD89138B217A7 FOREIGN KEY (publication_id) REFERENCES publication (id)');
@@ -32,10 +33,11 @@ final class Version20230303223119 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE commentaire DROP FOREIGN KEY FK_67F068BC38B217A7');
         $this->addSql('ALTER TABLE pub_like DROP FOREIGN KEY FK_57AD89138B217A7');
         $this->addSql('ALTER TABLE pub_like DROP FOREIGN KEY FK_57AD891A76ED395');
-        $this->addSql('ALTER TABLE commentaire DROP FOREIGN KEY FK_67F068BC38B217A7');
         $this->addSql('DROP TABLE commentaire');
+        $this->addSql('DROP TABLE pub_like');
         $this->addSql('DROP TABLE publication');
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE messenger_messages');
